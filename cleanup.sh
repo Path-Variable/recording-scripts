eae8ab1a2315d34b0e843eac7561f45906885f11
Ivan Šarić <ivan@path-variable.com>
2022-01-11 16:28:26 +0100

    fix errors in cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-## delete anything older than 3 days
+## delete anything older than 7 days
 ## assumes folder structure like /base/cameras/location/room
-find /base/path/to/recordings/*/*/*/ -type d -mtime +7 -exec rm -rf {} \;
+find /base/cameras/*/*/* -type f -mtime +7 -exec rm -rf {} \;
 
 
